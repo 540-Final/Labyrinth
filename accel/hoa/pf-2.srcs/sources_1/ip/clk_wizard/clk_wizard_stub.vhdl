87ac7f4fbae3a2b542d5da790188a59f99c88641
quachh <quach.hoa@gmail.com>
2014-12-05 12:19:48 -0800

    clean up

---
+++ accel/hoa/pf-2.srcs/sources_1/ip/clk_wizard/clk_wizard_stub.vhdl
@@ -0,0 +1,31 @@
+-- Copyright 1986-2014 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2014.4 (win64) Build 1071353 Tue Nov 18 18:29:27 MST 2014
+-- Date        : Fri Dec 05 11:58:08 2014
+-- Host        : Black-PC running 64-bit Service Pack 1  (build 7601)
+-- Command     : write_vhdl -force -mode synth_stub
+--               H:/Document/School/ece540/pfinal/pf-2/pf-2.srcs/sources_1/ip/clk_wizard/clk_wizard_stub.vhdl
+-- Design      : clk_wizard
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7a100tcsg324-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity clk_wizard is
+  Port ( 
+    clk_in1 : in STD_LOGIC;
+    clk_out1 : out STD_LOGIC;
+    reset : in STD_LOGIC;
+    locked : out STD_LOGIC
+  );
+
+end clk_wizard;
+
+architecture stub of clk_wizard is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clk_in1,clk_out1,reset,locked";
+begin
+end;
