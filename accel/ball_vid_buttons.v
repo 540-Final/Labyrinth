6275ead7526f1641d3e91accf4d65c5e17b7f820
quachh <quach.hoa@gmail.com>
2014-12-08 12:09:00 -0800

    hole and win behavior

--- accel/ball_vid_buttons.v
+++ accel/ball_vid_buttons.v
@@ -20,6 +20,8 @@ module Ball
 	parameter integer   NUM_PX_TO_CHECK         = 15,	// 
 	parameter integer   OFFSET                  = 8,		// From center pixel to edge that needs checked
 	parameter 			WALL					= 8'h26,
+	parameter 			HOLE					= 8'h49,
+	parameter 			WIN						= 8'hF9,
 	parameter           READ_DELAY              = 3
 )
 (
@@ -34,15 +36,16 @@ I need to figure out how to do that here in a way that makes sense. For now its 
 	input		[3:0]	movement,
 	
 	//These are the actual output coordinates of the ball, if it was able to move to a particular spot
-    output reg	[8:0]	y_out,
+    output reg	[9:0]	y_out,
 	output reg	[9:0]	x_out,
 	
 	input 		[9:0]	vid_row,		// video logic row address
 	input 		[9:0]	vid_col,		// video logic column address
 	output   	[7:0]	vid_pixel_out,	// pixel (location) value
+	output	reg			gameover,
 	
 	output [12:0] debug,
-	input update,
+	input update
 	output [7:0] px_result
 );
 
@@ -64,22 +67,11 @@ I need to figure out how to do that here in a way that makes sense. For now its 
 	reg [1:0] rom_read_delay;
 	reg [3:0] intended_movement_dir, check_px;
 	reg [9:0] x_move_check_addr, y_move_check_addr;
+
 	
 	wire [7:0] px_result;
 	
 	assign debug = {locked_intended_move, movement_validated, move_is_valid, rom_read_delay, intended_movement_dir, check_px};
-	
-/*	initial begin
-		locked_intended_move	<= NO;
-		movement_validated	 	<= NO;
-		move_is_valid			<= YES;
-		intended_movement_dir	<= 4'b0000;
-		rom_read_delay			<= 2'b00;
-		check_px				<= 4'b0000;
-		y_out					<= INITIAL_Y;
-		x_out					<= INITIAL_X;
-	end	*/
-	
 	
 	always @(posedge update) begin
 		if (reset_in) begin
@@ -88,86 +80,105 @@ I need to figure out how to do that here in a way that makes sense. For now its 
 			locked_intended_move	<= NO;
 			movement_validated	 	<= NO;
 			move_is_valid			<= YES;
+			gameover <= NO;
 			intended_movement_dir	<= 4'b0000;
 			rom_read_delay			<= 2'b00;
 			check_px				<= 4'b0000;
 		end
 		else begin
-			if (locked_intended_move) begin // We already have an intended direction
-				if (movement_validated) begin
-					if (move_is_valid) begin
-						case (intended_movement_dir)
-							UP 	    : y_out <= y_out - 1'b1;	
-							DOWN 	: y_out <= y_out + 1'b1;
-							LEFT 	: x_out <= x_out - 1'b1;
-							RIGHT	: x_out <= x_out + 1'b1;
-						endcase
+			if (!gameover) begin
+			
+				if (locked_intended_move) begin // We already have an intended direction
+					if (movement_validated) begin
+						if (move_is_valid) begin
+							case (intended_movement_dir)
+								UP 	    : y_out <= y_out - 1'b1;	
+								DOWN 	: y_out <= y_out + 1'b1;
+								LEFT 	: x_out <= x_out - 1'b1;
+								RIGHT	: x_out <= x_out + 1'b1;
+							endcase
+						end
+						movement_validated	 <= NO;
+						move_is_valid 		 <= YES;
+						locked_intended_move <= NO;
+						gameover <= NO;
 					end
-					movement_validated	 <= NO;
-					move_is_valid 		 <= YES;
-					locked_intended_move <= NO;
+					else begin
+						if (check_px < NUM_PX_TO_CHECK) begin // More pixels to scan
+							if (rom_read_delay == READ_DELAY) begin // Ready to read the world pixel
+								// TODO read the rom and set if move is valid
+								if(px_result == WALL) begin
+									move_is_valid 		<= NO;
+									movement_validated  <= YES;		// move is invalid, no need to keep scanning.
+									check_px		    <= 4'b0000;
+								end
+								else if (px_result == HOLE)begin
+								 	move_is_valid 		<= NO;
+									movement_validated  <= YES;		// move is invalid, no need to keep scanning.
+									check_px		    <= 4'b0000;
+									y_out <= INITIAL_Y;
+									x_out <= INITIAL_X;
+								end
+								else if ( px_result == WIN)begin
+									gameover <= YES;							
+								end
+								else begin 				// reset read delay
+									check_px		<= check_px + 1'b1;		// increment the check pixel
+								end
+               	             rom_read_delay    <= 2'b00;
+							end
+							else if (rom_read_delay	== 2'b00) begin
+								// Set the address of the next pixel to check
+								case (intended_movement_dir)
+									RIGHT:
+										begin
+											x_move_check_addr <= x_out + OFFSET;
+											y_move_check_addr <= y_out - OFFSET + 1'b1 + check_px;
+										end
+									LEFT:
+										begin
+											x_move_check_addr <= x_out - OFFSET;
+											y_move_check_addr <= y_out - OFFSET + 1'b1 + check_px;
+										end
+									UP:
+										begin
+											y_move_check_addr <= y_out - OFFSET;
+											x_move_check_addr <= x_out - OFFSET + 1'b1 + check_px;
+										end
+									 DOWN:
+										begin
+											y_move_check_addr <= y_out + OFFSET;
+											x_move_check_addr <= x_out - OFFSET + 1'b1 + check_px;
+										end
+								endcase
+								rom_read_delay = rom_read_delay + 1'b1; // start waiting for rom response
+							end
+							else rom_read_delay = rom_read_delay + 1'b1; // Waiting for rom response
+						end
+						else begin // Scanned all pixels
+							movement_validated <= YES;
+							check_px		   <= 4'b0000;
+						end
+					end
 				end
 				else begin
-					if (check_px < NUM_PX_TO_CHECK) begin // More pixels to scan
-						if (rom_read_delay == READ_DELAY) begin // Ready to read the world pixel
-							// TODO read the rom and set if move is valid
-							if(px_result == WALL) begin
-								move_is_valid 		<= NO;
-								movement_validated  <= YES;		// move is invalid, no need to keep scanning.
-								check_px		    <= 4'b0000;
-							end
-							else begin 				// reset read delay
-								check_px		<= check_px + 1'b1;		// increment the check pixel
-							end
-                            rom_read_delay    <= 2'b00;
-						end
-						else if (rom_read_delay	== 2'b00) begin
-							// Set the address of the next pixel to check
-							case (intended_movement_dir)
-								RIGHT:
-									begin
-										x_move_check_addr <= x_out + OFFSET;
-										y_move_check_addr <= y_out - OFFSET + 1'b1 + check_px;
-									end
-								 LEFT:
-									begin
-										x_move_check_addr <= x_out - OFFSET;
-										y_move_check_addr <= y_out - OFFSET + 1'b1 + check_px;
-									end
-								   UP:
-									begin
-										y_move_check_addr <= y_out - OFFSET;
-										x_move_check_addr <= x_out - OFFSET + 1'b1 + check_px;
-									end
-								 DOWN:
-									begin
-										y_move_check_addr <= y_out + OFFSET;
-										x_move_check_addr <= x_out - OFFSET + 1'b1 + check_px;
-									end
-							endcase
-							rom_read_delay = rom_read_delay + 1'b1; // start waiting for rom response
-						end
-						else rom_read_delay = rom_read_delay + 1'b1; // Waiting for rom response
+					if(|movement) begin
+						intended_movement_dir <= movement;
+						locked_intended_move <= YES;
+					end	
+					else begin
+						locked_intended_move <= NO;
+				    	x_move_check_addr <= x_out;
+				    	y_move_check_addr <= y_out;
 					end
-					else begin // Scanned all pixels
-						movement_validated <= YES;
-						check_px		   <= 4'b0000;
-					end
-				end		
+				end
 			end
 			else begin
-				if(|movement) begin
-					intended_movement_dir <= movement;
-					locked_intended_move <= YES;
-				end
-				else begin
-				    locked_intended_move <= NO;
-				    x_move_check_addr <= x_out;
-				    y_move_check_addr <= y_out;
-				end
-			end			
-		end
-	end
+				x_move_check_addr <= x_move_check_addr;
+				y_move_check_addr <= y_move_check_addr;
+			end
+		end		
+	end	
 	
 	map amap (
 		.clk (clk),
